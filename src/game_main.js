582f47c5e7b78a17b4bc7f70f3f74761f14455d3
Jeremy Lawrence <sqlsherpa@gmail.com>
2019-09-01 09:05:32 -0600

    Sprite movement within bounds
    Testing sprite movement bouncing and wrapping through the canvas

--- src/game_main.js
+++ src/game_main.js
@@ -4,22 +4,34 @@ let {init, Sprite, GameLoop} = kontra //create the kontra objects
 
 let { canvas } = init();
 
+
+
+
 //create a sprite object
 let sprite = Sprite({
 	x:0,
-	y: 0,
+	y:0,
 	width: 50,
 	height: 100,
-	color: 'green',
-	dx:1,
-	dy:0.5
+	color: 'blue',
+	dx:2
 });
 
-//game loop 60fps
-
+//game loop
 let loop = GameLoop({
 	update: function(){
 		sprite.update();
+
+		//wraps the sprite through the canvas
+		// if(sprite.x > canvas.width){
+		// 	sprite.x = -sprite.width;
+		// }
+
+		//bounces inside the canvas
+		if(((sprite.width + sprite.x) > canvas.width) || (sprite.x < 0))
+		{
+			sprite.dx = -sprite.dx
+		}
 	},
 	render: function(){
 		sprite.render();
