4dd90b5901eab30175d5fb89b66432037916f1d1
Jeremy Lawrence <sqlsherpa@gmail.com>
2019-09-03 11:00:16 -0600

    Scaling things down, working on circular motion

--- src/game_main.js
+++ src/game_main.js
@@ -6,24 +6,39 @@ let { canvas } = init();
 
 let canvasWidthMid = canvas.width/2;
 let canvasHeightMid = canvas.height/2;
+let earthRadius = 75;
 
-//bring in an image
+//bring in the assets
 let chickImg = new Image();
-chickImg.src = 'assets/sprites/chick.png';
+chickImg.src = 'assets/sprites/Chick@16px.png';
+
+let motherBirdStandRightImg = new Image();
+motherBirdStandRightImg.src = 'assets/sprites/MotherBirdStandRight@16px.png';
+
+let emptyNestImg = new Image();
+emptyNestImg.src = 'assets/sprites/emptyNest@32px.png';
 
 let chickSprite = Sprite({
 		x:0,
 		y:0,
-		dx:2,
-		dy:1,
+		dx:.25,
+		dy:.5,
 		image: chickImg
+	});
+
+let motherBirdStandRightSprite = Sprite({
+		x:200,
+		y:100,
+		dx:.75,
+		dy:.25,
+		image: motherBirdStandRightImg
 	});
 
 let earthSprite = Sprite({
 		x:canvasWidthMid,
 		y:canvasHeightMid,
 		color: 'YellowGreen',
-		radius:50,
+		radius:earthRadius,
 		render: function(){
 			this.context.fillStyle = this.color;
 			this.context.beginPath();
@@ -32,12 +47,31 @@ let earthSprite = Sprite({
 		}
 	});
 
+let emptyNestSprite = Sprite({
+		x:earthSprite.x ,
+		y:earthSprite.y - earthSprite.radius,
+		anchor: {x:.5,y:1},
+		image: emptyNestImg
+	});
+
+let thetaRad = 0;
+let thetaDeg = 0;
+
 //game loop
 let loop = GameLoop({
 	update: function(){
-		chickSprite.update();
 		earthSprite.update();
+		chickSprite.update();
+		motherBirdStandRightSprite.update();
+		emptyNestSprite.update();
 
+		//rotation
+		thetaRad = thetaRad + 0.025;
+		thetaDeg = thetaRad * (180/Math.PI) ;
+		
+		emptyNestSprite.x = canvasWidthMid + earthRadius * Math.cos(thetaRad);
+		emptyNestSprite.y = canvasHeightMid + earthRadius * Math.sin(thetaRad);
+		emptyNestSprite.rotation = thetaDeg
 		//wraps the sprite through the canvas x axis
 		// if(chickSprite.x > canvas.width){
 		// 	chickSprite.x = -chickSprite.width;
@@ -61,11 +95,20 @@ let loop = GameLoop({
 			chickSprite.y = -chickSprite.height;
 		}
 
+		if(motherBirdStandRightSprite.x > canvas.width){
+			motherBirdStandRightSprite.x = -motherBirdStandRightSprite.width;
+		}
+		if(motherBirdStandRightSprite.y > canvas.height){
+			motherBirdStandRightSprite.y = -motherBirdStandRightSprite.height;
+		}
+
 
 	},
 	render: function(){
-		chickSprite.render();
 		earthSprite.render();
+		chickSprite.render();
+		motherBirdStandRightSprite.render();
+		emptyNestSprite.render();
 	}
 })
 
