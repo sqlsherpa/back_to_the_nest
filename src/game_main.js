9dca0f47f64758a2b830e887cf541f0ba1decd1b
Jeremy Lawrence <sqlsherpa@gmail.com>
2019-09-04 14:42:02 -0600

    Asset promise
    Reorganized to use asset load promise, removed earth rotation mechanics for simplicity and time constraints

--- src/game_main.js
+++ src/game_main.js
@@ -1,116 +1,100 @@
 
 
-let {init, Sprite, GameLoop} = kontra //create the kontra objects
+let {init, setImagePath, load, on, imageAssets, Sprite, GameLoop} = kontra //create the kontra objects
 
 let { canvas } = init();
 
+//Setup the screen
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
 let canvasWidthMid = canvas.width/2;
 let canvasHeightMid = canvas.height/2;
-let earthRadius = 75;
 
 //bring in the assets
-let chickImg = new Image();
-chickImg.src = 'assets/sprites/Chick@16px.png';
+let numAssets = 3;
+let assetsLoaded = 0;
+on('assetLoaded', (asset,url) => {
+	assetsLoaded++;
+	console.log("#" + assetsLoaded + " asset loaded");
+});
 
-let motherBirdStandRightImg = new Image();
-motherBirdStandRightImg.src = 'assets/sprites/MotherBirdStandRight@16px.png';
+setImagePath('assets/sprites');
+load(
+	'Chick.png',
+	'MotherBirdStandRight.png',
+	'EmptyNest.png'
+	).then(function(assets){
+		console.log("All Assets Successfully Loaded.");
 
-let emptyNestImg = new Image();
-emptyNestImg.src = 'assets/sprites/emptyNest@32px.png';
+		//Start assigning files to objects
+		let chickSprite = Sprite({
+				x:0,
+				y:0,
+				dx:.25,
+				dy:.5,
+				image: imageAssets['Chick']
+			});
 
-let chickSprite = Sprite({
-		x:0,
-		y:0,
-		dx:.25,
-		dy:.5,
-		image: chickImg
+		let motherBirdStandRightSprite = Sprite({
+				x:200,
+				y:100,
+				dx:.75,
+				dy:.25,
+				image: imageAssets['MotherBirdStandRight']
+			});
+
+		let emptyNestSprite = Sprite({
+				x:500 ,
+				y:500 ,
+				anchor: {x:.5,y:1},
+				image: imageAssets['EmptyNest']
+			});
+
+		//game loop
+		let loop = GameLoop({
+			update: function(){
+
+				//Full Screen resize update
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight;
+
+				//Derived sizes by screen
+				canvasWidthMid = canvas.width/2;
+				canvasHeightMid = canvas.height/2;
+				
+				//Sprite Updates
+				chickSprite.update();
+				motherBirdStandRightSprite.update();
+				emptyNestSprite.update();
+
+				//wraps the sprite through the canvas x and y axis
+				if(chickSprite.x > canvas.width){
+					chickSprite.x = -chickSprite.width;
+				}
+				if(chickSprite.y > canvas.height){
+					chickSprite.y = -chickSprite.height;
+				}
+
+				if(motherBirdStandRightSprite.x > canvas.width){
+					motherBirdStandRightSprite.x = -motherBirdStandRightSprite.width;
+				}
+				if(motherBirdStandRightSprite.y > canvas.height){
+					motherBirdStandRightSprite.y = -motherBirdStandRightSprite.height;
+				}
+
+
+			},
+			render: function(){
+				chickSprite.render();
+				motherBirdStandRightSprite.render();
+				emptyNestSprite.render();
+			}
+		});
+
+
+		loop.start();
+
+
 	});
 
-let motherBirdStandRightSprite = Sprite({
-		x:200,
-		y:100,
-		dx:.75,
-		dy:.25,
-		image: motherBirdStandRightImg
-	});
-
-let earthSprite = Sprite({
-		x:canvasWidthMid,
-		y:canvasHeightMid,
-		color: 'YellowGreen',
-		radius:earthRadius,
-		render: function(){
-			this.context.fillStyle = this.color;
-			this.context.beginPath();
-			this.context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-			this.context.fill();
-		}
-	});
-
-let emptyNestSprite = Sprite({
-		x:earthSprite.x ,
-		y:earthSprite.y - earthSprite.radius,
-		anchor: {x:.5,y:1},
-		image: emptyNestImg
-	});
-
-let thetaRad = 0;
-let thetaDeg = 0;
-
-//game loop
-let loop = GameLoop({
-	update: function(){
-		earthSprite.update();
-		chickSprite.update();
-		motherBirdStandRightSprite.update();
-		emptyNestSprite.update();
-
-		//rotation
-		thetaRad = thetaRad + 0.025;
-		thetaDeg = thetaRad * (180/Math.PI) ;
-		
-		emptyNestSprite.x = canvasWidthMid + earthRadius * Math.cos(thetaRad);
-		emptyNestSprite.y = canvasHeightMid + earthRadius * Math.sin(thetaRad);
-		emptyNestSprite.rotation = thetaDeg
-		//wraps the sprite through the canvas x axis
-		// if(chickSprite.x > canvas.width){
-		// 	chickSprite.x = -chickSprite.width;
-		// }
-
-		//bounces inside the canvas
-		// if(((chickSprite.width + chickSprite.x) > canvas.width) || (chickSprite.x < 0))
-		// {
-		// 	chickSprite.dx = -chickSprite.dx
-		// }
-		// if(((chickSprite.height + chickSprite.y) > canvas.height) || (chickSprite.y < 0))
-		// {
-		// 	chickSprite.dy = -chickSprite.dy
-		// }
-
-		//wraps the sprite through the canvas x and y axis
-		if(chickSprite.x > canvas.width){
-			chickSprite.x = -chickSprite.width;
-		}
-		if(chickSprite.y > canvas.height){
-			chickSprite.y = -chickSprite.height;
-		}
-
-		if(motherBirdStandRightSprite.x > canvas.width){
-			motherBirdStandRightSprite.x = -motherBirdStandRightSprite.width;
-		}
-		if(motherBirdStandRightSprite.y > canvas.height){
-			motherBirdStandRightSprite.y = -motherBirdStandRightSprite.height;
-		}
-
-
-	},
-	render: function(){
-		earthSprite.render();
-		chickSprite.render();
-		motherBirdStandRightSprite.render();
-		emptyNestSprite.render();
-	}
-})
-
-
-loop.start();
